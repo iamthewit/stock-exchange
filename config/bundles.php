58383f650f788414d401dfa3f3309518cf6570e7
Ben Cross <bencross86@gmail.com>
2021-08-05 14:07:43 +0100

    WIP: working on testing JSON

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,7 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class => ['dev' => true, 'test' => true],
+    Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
 ];
--- tests/Infrastructure/Http/Controller/TradeControllerTest.php
+++ tests/Infrastructure/Http/Controller/TradeControllerTest.php
@@ -2,25 +2,56 @@
 
 namespace StockExchange\Tests\Infrastructure\Http\Controller;
 
+use ApiTestCase\JsonApiTestCase;
+use Coduo\PHPMatcher\Matcher\JsonMatcher;
+use Coduo\PHPMatcher\PHPMatcher;
+use Kint\Kint;
 use StockExchange\Infrastructure\Http\Controller\TradeController;
 use PHPUnit\Framework\TestCase;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class TradeControllerTest extends WebTestCase
+class TradeControllerTest extends JsonApiTestCase
 {
     public function testItDoesSomething()
     {
-        $this->markTestIncomplete();
+//        $this->markTestIncomplete();
         
         // This calls KernelTestCase::bootKernel(), and creates a
         // "client" that is acting as the browser
-        $client = static::createClient();
+//        $client = static::createClient();
 
         // Request a specific page
-        $crawler = $client->request('GET', '/trade');
+        $this->client->request('GET', '/trader');
 
         // Validate a successful response and some content
 //        $this->assertResponseIsSuccessful();
-//        $this->assertSelectorTextContains('h1', 'Hello World');
+//        $this->assertJson($client->getResponse()->getContent());
+
+//        $this->assertResponse(
+//            $this->client->getResponse(),
+//            'trader/index'
+//        );
+
+        $matcher = new PHPMatcher();
+
+        $match = $matcher->match(
+            $this->client->getResponse()->getContent(),
+            '
+                [
+                  {
+                    "id": @uuid@,
+                    "shares": @array@
+                  },
+                  @...@
+                ]
+                '
+        );
+
+//        $this->assertTrue(
+//            $match
+//        );
+
+        d($matcher->error());
+
     }
 }
