0cdf66308dcd6b494b97bf67ad640d521c481ff2
Ben Cross <bencross86@gmail.com>
2021-08-13 17:18:45 +0100

    Separated seeding and db migration shell scripts

---
+++ create-and-migrate.event-streams.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# drop the database
+docker exec -i docker-event_streams_mysql_1 mysqladmin --user=root --password=root --force drop event_streams
+
+# create the database
+docker exec -i docker-event_streams_mysql_1 mysql --user=root --password=root -e "CREATE DATABASE event_streams"
+
+# run the event stream migrations
+docker exec -i docker-event_streams_mysql_1 mysql event_streams --user=root --password=root < ./config/scripts/mysql/01_event_streams_table.sql
+docker exec -i docker-event_streams_mysql_1 mysql event_streams --user=root --password=root < ./config/scripts/mysql/02_projections_table.sql
--- seed-mysql-event-streams.sh
+++ seed-mysql-event-streams.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 
-# drop the database
-docker exec -i docker-event_streams_mysql_1 mysqladmin --user=root --password=root --force drop event_streams
-
-# create the database
-docker exec -i docker-event_streams_mysql_1 mysql --user=root --password=root -e "CREATE DATABASE event_streams"
-
-# run the event stream migrations
-docker exec -i docker-event_streams_mysql_1 mysql event_streams --user=root --password=root < ./config/scripts/mysql/01_event_streams_table.sql
-docker exec -i docker-event_streams_mysql_1 mysql event_streams --user=root --password=root < ./config/scripts/mysql/02_projections_table.sql
-
 # seed the database
 ./bin/console seed:event-stream-db
